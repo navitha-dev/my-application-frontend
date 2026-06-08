3c32371b6f28e06eb6a0e7e6af6c9e72142a731e
Ramakrishna <navithajune06@gmail.com>
2026-06-08 12:02:02 +0530

    rk-1

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://my-application-backend-rk.ramak3488.workers.dev/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://my-application-backend.navithajune06.workers.dev/api';
 
 // Create axios instance
 const api = axios.create({
